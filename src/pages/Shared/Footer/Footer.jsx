36dfe97e604686e5b7f89c67e435a56771692d4c
Talha <mdtalha2008@gmail.com>
2024-03-24 17:48:23 +0600

    framer add

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import { BsTwitterX } from "react-icons/bs";
+import { FaWhatsapp } from "react-icons/fa";
+import { motion } from "framer-motion";
+
 
 const Footer = () => {
   return (
@@ -119,63 +127,76 @@ const Footer = () => {
                 @knyttneve
               </a>
             </p>
-            <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <a class="text-gray-500">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                </svg>
-              </a>
-              <a class="ml-3 text-gray-500">
-                <svg
-                  fill="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                </svg>
-              </a>
-              <a class="ml-3 text-gray-500">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                </svg>
-              </a>
-              <a class="ml-3 text-gray-500">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="0"
-                  class="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="none"
-                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                  ></path>
-                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
-                </svg>
-              </a>
-            </span>
+            <div class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <div className="grid grid-flow-col gap-3">
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaFacebookF size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaInstagram size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaLinkedinIn size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <BsTwitterX size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaWhatsapp size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaDiscord size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaThreads size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <FaYoutube size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+            <motion.div whileTap={{ rotate: 90, scale: 0.75 }}>
+              <Link to="">
+                <a href="">
+                  <BiLogoGmail size="1.7rem" color="black" />
+                </a>
+              </Link>
+            </motion.div>
+          </div>
+            </div>
           </div>
         </div>
       </footer>
